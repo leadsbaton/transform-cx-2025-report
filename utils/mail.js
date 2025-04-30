b906044dd634caf81314a9adce5b1e9d8324254e
Biswajit Das <djdas000000@gmail.com>
2025-04-30 22:30:16 +0530

    Initial clean commit

---
+++ utils/mail.js
@@ -0,0 +1,12 @@
+// // utils/mail.js
+// export const EMAIL_SERVICE_ID = "service_otp1csw";
+// export const EMAIL_TEMPLATE_ID = "template_1mewapl";
+// export const EMAIL_PUBLIC_KEY = "9Qlr97COTksQ3suqK"; // Replace with actual public key
+
+
+// utils/mail.js
+export const EMAIL_SERVICE_ID = "service_6usakhn";
+export const EMAIL_TEMPLATE_ID = "template_001go4c";
+export const EMAIL_PUBLIC_KEY = "bVJGqgbuDB6REerGp"; // Replace with actual public key
+
+
